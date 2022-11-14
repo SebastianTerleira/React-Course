26eae45d40efb7b25483712dde22d202f827c9dd
Martha Huaman <ms.huamann@gmail.com>
2022-11-13 21:20:48 -0500

    ADD: init config

---
+++ contact-app/src/components/AddContact.js
@@ -0,0 +1,11 @@
+// import React from "react";
+
+// class AddContact extends React.Component{
+//   render() {
+//     return (
+//       <div className=""></>
+//     );
+//   }
+// }
+
+// export default AddContact;
